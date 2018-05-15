8fce0448865aa97b7d8a96e2ac81ce5ec7e1a56f
wangyingli.102 <wangyingli.102@bytedance.com>
2018-05-15 17:26:19 +0800

    替换系统的webview.apk 但是发现高版本没有HardwareCanvas，在初始化插件过程中crash，应该是要对不同的版本进行兼容处理，成本比较高
    Change-Id: Ibe5de1580e094a5d3f07d873b137054ca40bc9d2

--- app/src/main/java/webview/bytedance/com/mywebview/MainActivity.java
+++ app/src/main/java/webview/bytedance/com/mywebview/MainActivity.java
@@ -2,6 +2,7 @@ package webview.bytedance.com.mywebview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.bytewebkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) this.findViewById(R.id.webView);
+        webView.loadUrl("https://github.com/HomHomLin");
     }
 }
